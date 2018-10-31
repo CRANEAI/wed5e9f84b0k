a1edc53fd332920ccd8b6ff822995d97ef3e228b
Elly <elly@mg.crane.ai>
2018-10-31 11:09:44 -0400

    added tests/signup refactoring

---
+++ tests/signup.test.js
@@ -0,0 +1,21 @@
+/* 
+ * Contact: team@fusioncode.ai
+ *
+ *
+ * Project  - signup UNIT TESTS
+ * Application Client
+ * Wed
+*/
+
+/* view has actions which trigger on events, view should should error messages and display activity indicator when processing */
+it("view has actions which trigger on events, view should should error messages and display activity indicator when processing", () => {
+  /* ToDo developer needs to complete unit test 
+               view has actions which trigger on events, view should should error messages and display activity indicator when processing
+               uncomment below and update the code to test
+                //instance.action(function(error) {
+                //  if (error) throw error;
+                //expect().to.be();
+                //});
+                done();
+               */
+})
